d3a3b42998a7a5e7cb8e44f5cc5cdceb0580f696
Suman Biswas <sumanmanner@gmail.com>
2023-04-07 00:09:13 +0530

    updated

--- hunchers/src/AllRoutes/AllRoutes.tsx
+++ hunchers/src/AllRoutes/AllRoutes.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import Navbar from "../components/Navbar";
 
 const AllRoutes = () => {
   return (
-    <Routes>
-      <Route path={"/"} element={<HomePage />} />
-    </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+      </Routes>
+    </div>
   );
 };
 
--- hunchers/src/components/Navbar.tsx
+++ hunchers/src/components/Navbar.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { FaUser } from "react-icons/fa";
+import { BsStopwatchFill, BsEmojiHeartEyesFill } from "react-icons/bs";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex justify-between">
+      <div className="p-3 pt-2 flex gap-3 place-items-center">
+        <button className="bg-yellow-300 rounded-2xl">
+          <div className="flex place-items-center gap-4 text-xl p-2 pr-4 pl-4">
+            <p>Players</p>
+            <FaUser />
+          </div>
+        </button>
+        <button className="p-3 pt-2 bg-yellow-300 rounded-2xl flex place-items-center text-xl">
+          <BsStopwatchFill />
+        </button>
+      </div>
+      <div className="flex gap-3 place-items-center p-3 pt-2">
+        <button className="bg-yellow-300 rounded-2xl text-xl p-2 pr-4 pl-4">
+          Rules
+        </button>
+        <button className="flex gap-2 place-items-center bg-[#DBD4AF] rounded-2xl text-xl p-2 pr-4 pl-4">
+          <div>Hiru</div>
+          <BsEmojiHeartEyesFill />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
