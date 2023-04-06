a83cf52b7adaf98fa9261bb76b415449381b05e4
Suman Biswas <sumanmanner@gmail.com>
2023-04-07 00:35:26 +0530

    updated

--- hunchers/src/AllRoutes/AllRoutes.tsx
+++ hunchers/src/AllRoutes/AllRoutes.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
-import Navbar from "../components/Navbar";
 
 const AllRoutes = () => {
   return (
     <div>
-      <Navbar />
       <Routes>
         <Route path={"/"} element={<HomePage />} />
       </Routes>
