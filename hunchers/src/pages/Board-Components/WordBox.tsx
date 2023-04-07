da5de9efecc5a96da947bed71136142b6cdcbb13
varun8177 <varunergurala9999@gmail.com>
2023-04-07 19:02:16 +0530

    updated wordbox styling

--- hunchers/src/pages/Board-Components/WordBox.tsx
+++ hunchers/src/pages/Board-Components/WordBox.tsx
@@ -1,21 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 
 const WordBox = () => {
     const controls = useAnimation();
-    let arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    let arr: { word: string, definition: string }[] = [{ "word": "Abundant", "definition": "present in great quantity; more than adequate; oversufficient" }, { "word": "Bellicose", "definition": "inclined or eager to fight; aggressively hostile; belligerent" }, { "word": "Cacophony", "definition": "harsh discordance of sound; dissonance; a harsh, discordant mixture of sounds" }, { "word": "Dichotomy", "definition": "division into two parts, kinds, etc.; subdivision into halves or pairs" }, { "word": "Euphemism", "definition": "an inoffensive or indirect expression that is substituted for one that is considered offensive or too harsh" }, { "word": "Facetious", "definition": "not meant to be taken seriously or literally; humorous" }, { "word": "Galvanize", "definition": "to stimulate or excite as if by an electric shock; to startle into sudden activity or action" }, { "word": "Hegemony", "definition": "leadership or predominant influence exercised by one nation over others, as in a confederation" }, { "word": "Immutable", "definition": "not subject to change; constant; unalterable" }, { "word": "Juxtapose", "definition": "to place close together or side by side, especially for comparison or contrast" }, { "word": "Kudos", "definition": "praise and honor received for an achievement" }, { "word": "Languid", "definition": "lacking energy or vitality; weak or sluggish" }, { "word": "Maelstrom", "definition": "a powerful whirlpool in the sea or a river; a situation or state of confused movement or violent turmoil" }, { "word": "Nadir", "definition": "the lowest point; the point of greatest adversity or despair" }, { "word": "Omnipotent", "definition": "having unlimited power; able to do anything" }, { "word": "Pernicious", "definition": "causing harm or ruin; injurious; hurtful" }];
+    const [current, setCurrent] = useState<number | null>(null);
+
     useEffect(() => {
         controls.start("animate");
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-    const gridItems = arr.map((_, index) => (
+    const gridItems = arr.map((item, index) => (
         <motion.div
             key={index}
             initial={{ y: -1000 }}
             animate={{ y: 0, transition: { delay: index * 0.1 } }}
-            className={`h-24 w-36 rounded-md bg-[#f4d8b5] grid-row-${Math.floor(index / 4) + 1
-                } grid-column-${(index % 4) + 1}`}
-        ></motion.div>
+            className={`h-24 w-36 rounded-md place-items-center bg-[#f4d8b5] grid-row-${Math.floor(index / 4) + 1} grid-column-${(index % 4) + 1} flex justify-center`}
+        >
+            <div className={`border-2 rounded-md p-2 border-[#b19578] w-[90%] h-[90%] flex flex-col justify-center text-center ${index === current ? 'bg-red-500' : "none"}`} onClick={() => {
+                setCurrent(index)
+            }}>
+                <div className="flex flex-row-reverse place-items-end mb-2 justify-between">
+                    <img className="w-[40%]" src="https://cdn.codenames.game/v20210210/theme/classic/card/black-back.png" alt="detective" />
+                    <div className="w-[50%] border-b-2 border-[#b19578] ">
+
+                    </div>
+                </div>
+                <p className="bg-white w-[100%]">
+                    {item.word}
+                </p>
+            </div>
+        </motion.div >
     ));
 
     return <div className="grid fixed grid-cols-4 gap-x-5 gap-y-5 left-[30%] top-[12%] ">{gridItems}</div>;
