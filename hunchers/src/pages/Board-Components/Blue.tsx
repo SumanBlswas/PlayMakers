5a65c58da63df0929f48712d99f2a8d50c0cc765
Suman Biswas <sumanmanner@gmail.com>
2023-04-07 16:27:18 +0530

    updated

--- hunchers/src/pages/Board-Components/Blue.tsx
+++ hunchers/src/pages/Board-Components/Blue.tsx
@@ -4,11 +4,11 @@ const Blue = () => {
   return (
     <div className="bg-[#3284a3]  fixed right-0 p-5 rounded-2xl m-4">
       <div className="flex justify-between place-items-center gap-8">
+        <p className="text-white text-4xl font-bold">8</p>
         <img
           src="https://user-images.githubusercontent.com/112753516/230549426-edff759b-78cf-4ea2-b87d-2de5300d55f0.png"
           alt="red"
         />
-        <p className="text-white text-4xl font-bold">8</p>
       </div>
       <div className="text-white flex gap-5 flex-col pt-2">
         <div className="flex flex-col max-w-[65%] gap-3">
