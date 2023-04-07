c164d9ebaa1af73c8b840fe997471a7ef21ead88
Suman Biswas <sumanmanner@gmail.com>
2023-04-07 11:35:17 +0530

    updated

--- hunchers/src/AllRoutes/AllRoutes.tsx
+++ hunchers/src/AllRoutes/AllRoutes.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import Board from "../pages/Board";
+import Navbar from "../components/Navbar";
 
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path={"/"} element={<HomePage />} />
+        <Route
+          path={"/board"}
+          element={
+            <>
+              <Navbar />
+              <Board />
+            </>
+          }
+        />
       </Routes>
     </div>
   );
--- hunchers/src/pages/Board.tsx
+++ hunchers/src/pages/Board.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Red from "./Board-Components/Red";
+import Blue from "./Board-Components/Blue";
 
 const Board = () => {
   return (
-    <div>Board</div>
-  )
-}
+    <div className="fixed top-[10%]">
+      <Red />
+      <Blue />
+    </div>
+  );
+};
 
-export default Board
+export default Board;
