e767a8456b5d4b4bb99e3ca1fd9156e8951f4184
Nisha Sharma <Nisha Sharma>
2023-04-09 00:00:19 +0530

    Backend code

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,64 @@
-const express = require("express");
+const express=require("express")
+const cors=require("cors")
+const {StreamChat}=require("stream-chat")
+const {v4} =require("uuid")
+const bcrypt=require("bcrypt")
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+const api_key = "dnkx7pvtvsqg";
+const api_secret =
+  "98dhq3e3qa3q6fx3nxdnwq3fyvbc6nrd6sq2vbg5ctwxdmvzpd29fxmb4ngva72b";
+
+const serverClient = StreamChat.getInstance(api_key, api_secret);
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { firstName, lastName, username, password } = req.body;
+    const userId = v4();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const token = serverClient.createToken(userId);
+    res.json({ token, userId, firstName, lastName, username, hashedPassword });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const { users } = await serverClient.queryUsers({ name: username });
+    if (users.length === 0) return res.json({ message: "User not found" });
+
+    const token = serverClient.createToken(users[0].id);
+    const passwordMatch = await bcrypt.compare(
+      password,
+      users[0].hashedPassword
+    );
+
+    if (passwordMatch) {
+      res.json({
+        token,
+        firstName: users[0].firstName,
+        lastName: users[0].lastName,
+        username,
+        userId: users[0].id,
+      });
+    }
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
+
+
+
+
+/*const express = require("express");
 const connection = require("./configs/db");
 const cors = require("cors");
 const userRouter = require("./routes/userRoutes");
@@ -46,4 +106,4 @@ app.listen(8080, async () => {
     console.log(error.message);
   }
   console.log(`Connected the server at 8080`);
-});
+});*/
--- hunchers/src/App.tsx
+++ hunchers/src/App.tsx
@@ -1,7 +1,62 @@
-import AllRoutes from "./AllRoutes/AllRoutes";
+import "./App.css";
+import Login from "./components/Auth/Login";
+import SignUp from "./components/Auth/Signup";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import { useState } from "react";
+import JoinGame from "./components/JoinGame";
 
-export default function App() {
-  return <div className="bg-gradient-to-b from-[#0f0c29] via-[#6060d5] to-[#0f0c29] h-screen">
-    <AllRoutes />;
-  </div>
+function App() {
+  const api_key = "dnkx7pvtvsqg";
+  const cookies = new Cookies();
+  const token = cookies.get("token");
+  const client = StreamChat.getInstance(api_key);
+  const [isAuth, setIsAuth] = useState(false);
+
+  const logOut = () => {
+    cookies.remove("token");
+    cookies.remove("userId");
+    cookies.remove("firstName");
+    cookies.remove("lastName");
+    cookies.remove("hashedPassword");
+    cookies.remove("channelName");
+    cookies.remove("username");
+    client.disconnectUser();
+    setIsAuth(false);
+  };
+
+  if (token) {
+    client
+      .connectUser(
+        {
+          id: cookies.get("userId"),
+          name: cookies.get("username"),
+          firstName: cookies.get("firstName"),
+          lastName: cookies.get("lastName"),
+          hashedPassword: cookies.get("hashedPassword"),
+        },
+        token
+      )
+      .then(() => {
+        setIsAuth(true);
+      });
+  }
+  return (
+    <div className="App">
+      {isAuth ? (
+        <Chat client={client}>
+          <JoinGame />
+          <button onClick={logOut}> Log Out</button>
+        </Chat>
+      ) : (
+        <>
+          <SignUp setIsAuth={setIsAuth} />
+          <Login setIsAuth={setIsAuth} />
+        </>
+      )}
+    </div>
+  );
 }
+
+export default App;
