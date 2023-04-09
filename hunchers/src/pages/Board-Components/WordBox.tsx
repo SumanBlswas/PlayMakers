4209533c10bff461268ecc3c853601cc46920dde
varun8177 <varunergurala9999@gmail.com>
2023-04-09 14:03:56 +0530

    updated wordBox.tsx

--- hunchers/src/pages/Board-Components/WordBox.tsx
+++ hunchers/src/pages/Board-Components/WordBox.tsx
@@ -18,46 +18,44 @@ const WordBox = () => {
   const dispatch = useAppDispatch();
   const controls = useAnimation();
   let arr = useAppSelector((state) => state.boardReducer.words);
-
+  const [words, setWords] = useState(arr)
+  console.log("members", channel?.data?.member_count)
+  console.log(arr)
+  const [hideClue, setHideClue] = useState(false)
+  // const [def, setDef] = useState("")
   const [current, setCurrent] = useState<number | null>(null);
+  const [currentTurn, setCurrentTurn] = useState<"player1" | "player2">("player1");
+  const [currentClue, setCurrentClue] = useState<string>("");
 
-  // const ChooseSquare = async (word: string, index: number) => {
-  //   setCurrent(index);
-  //   setSelectedWord(word);
-  //   await channel.sendEvent({
-  //     type: "game-move" as EventTypes,
-  //     data: { selectedWord, index },
-  //   });
-  // };
-
-  const ChooseSquare = async (word: string, index: number) => {
-    setCurrent(index);
-    setSelectedWord(word);
-    await channel.sendEvent({
-      type: "game-move" as EventTypes,
-      data: { selectedWord, index },
-    });
+  const ChooseSquare = async (word: string, index: number, definition: string) => {
+    if (currentTurn === "player1") {
+      await channel.sendEvent({
+        type: "game-move" as EventTypes,
+        data: { selectedWord: word, index },
+      });
+      setCurrentTurn("player2");
+    }
   };
+
 
   useEffect(() => {
     controls.start("animate");
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  const gridItems = arr?.map((item: { word: string }, index: number) => (
+  const gridItems = arr?.map((item: { word: string, definition: string }, index: number) => (
     <motion.div
       key={index}
       initial={{ y: -1000 }}
       animate={{ y: 0, transition: { delay: index * 0.1 } }}
-      className={`h-24 w-36 rounded-md place-items-center bg-[#f4d8b5] grid-row-${
-        Math.floor(index / 4) + 1
-      } grid-column-${(index % 4) + 1} flex justify-center`}
+      className={`h-24 w-36 rounded-md place-items-center bg-[#f4d8b5] grid-row-${Math.floor(index / 4) + 1
+        } grid-column-${(index % 4) + 1} flex justify-center`}
     >
       <div
-        className={`border-2 rounded-md p-2 border-[#b19578] w-[90%] h-[90%] flex flex-col justify-center text-center ${
-          index === current ? "bg-red-500" : "none"
-        }`}
+        className={`border-2 rounded-md p-2 border-[#b19578] w-[90%] h-[90%] flex flex-col justify-center text-center ${index === current ? "bg-red-500" : "none"
+          }`}
         onClick={() => {
-          ChooseSquare(item.word, index);
+          // setDef(item.definition)
+          ChooseSquare(item.word, index, item.definition);
         }}
       >
         <div className="flex flex-row-reverse place-items-end mb-2 justify-between">
@@ -69,6 +67,9 @@ const WordBox = () => {
           <div className="w-[50%] border-b-2 border-[#b19578] "></div>
         </div>
         <p className="bg-white w-[100%]">{item.word}</p>
+        {/* {hideClue ? <button onClick={() => {
+          setHideClue(!hideClue)
+        }}>Clue</button> : <p className="bg-white w-[100%]">{item.definition}</p>} */}
       </div>
     </motion.div>
   ));
@@ -118,24 +119,54 @@ const WordBox = () => {
   //   }
   // });
 
-  channel.on((event) => {
-    if (event.type === ("game-move" as EventTypes)) {
-      const eventData = event.data as GameMoveEventData;
-      const { selectedWord, index } = eventData;
-      let x = arr.map((item: { word: string }) => {
-        return item.word === selectedWord ? { word: "Varun" } : item;
-      });
-      setCurrent(index);
-      setSelectedWord(selectedWord);
-      dispatch({ type: GET_WORDS, payload: x });
+  channel.on((event: { type: string; data?: { selectedWord: string; index: number } }) => {
+    if (event.type === "game-move" as EventTypes) {
+      if (event.data) {
+        // let count = 0;
+        const { selectedWord, index } = event?.data;
+        const updatedWords = [...words];
+        // setDef(def)
+        if (hideClue) {
+          console.log(event.data)
+          console.log(hideClue)
+          setHideClue(false)
+        }
+        setWords(updatedWords);
+
+        setCurrentTurn("player1");
+      }
     }
   });
 
+
   return (
-    <div className="grid fixed grid-cols-4 gap-x-5 gap-y-5 left-[30%] top-[12%] ">
-      {gridItems}
+    <div className="fixed left-[30%] top-[12%] ">
+      <div className="grid  grid-cols-4 gap-x-5 gap-y-5 left-[30%] top-[12%] ">
+        {gridItems}
+      </div>
+      {currentTurn === "player1" ? (
+        <div>
+          <input
+            placeholder="Enter a clue..."
+            onChange={(e) => setCurrentClue(e.target.value)}
+          />
+          {/* <select name="options" id="">
+            {arr.map((item: { definition: string, word: string }) => {
+              return <option value="">{item.definition}</option>
+            })}
+
+          </select> */}
+          <button onClick={() => setCurrentTurn("player2")}>Submit Clue</button>
+        </div>
+      ) : (
+        <div>
+          <p>Opponent is guessing</p>
+          {currentClue && <p>Clue: {currentClue}</p>}
+        </div>
+      )}
     </div>
   );
+
 };
 
 export default WordBox;
