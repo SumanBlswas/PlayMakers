53353fdc95d61506a77ed2584c755868a203b921
meenakshiar <meenakshiarora2707@gmail.com>
2023-04-09 22:50:08 +0530

    register page created

--- hunchers/src/components/Signup.tsx
+++ hunchers/src/components/Signup.tsx
@@ -29,34 +29,50 @@ function SignUp({ AuthToggle }: { AuthToggle: (value: Boolean) => void }) {
     );
   };
   return (
-    <div className="signUp">
-      <label> Sign Up</label>
-      <input
-        placeholder="First Name"
-        onChange={(event) => {
-          setUser({ ...user, firstName: event.target.value });
-        }}
+    <div className="flex justify-end items-center">
+      <div className="signUp grid w-[350px] bg-[#DBD4AF] gap-5 py-4 px-7 rounded-2xl mr-[390px]">
+        <h1 className="text-2xl font-bold text-center">Register</h1>
+        <input
+          placeholder="First Name"
+          onChange={(event) => {
+            setUser({ ...user, firstName: event.target.value });
+          }}
+          className="rounded-lg p-3"
+        />
+        <input
+          placeholder="Last Name"
+          onChange={(event) => {
+            setUser({ ...user, lastName: event.target.value });
+          }}
+          className="rounded-lg p-3"
+        />
+        <input
+          placeholder="Username"
+          onChange={(event) => {
+            setUser({ ...user, username: event.target.value });
+          }}
+          className="rounded-lg p-3"
+        />
+        <input
+          placeholder="Password"
+          type="password"
+          onChange={(event) => {
+            setUser({ ...user, password: event.target.value });
+          }}
+          className="rounded-lg p-3"
+        />
+        <button
+          onClick={signUp}
+          className="rounded-full text-xl p-2 w-[200px] tracking-wider bg-yellow-300 text-black font-semibold hover:bg-yellow-400  shadow-md shadow-black ml-[50px]"
+        >
+          Register
+        </button>
+      </div>
+      <img
+        src="https://media-public.canva.com/MADoheTQTsg/1/thumbnail.png"
+        alt=""
+        className="pt-40"
       />
-      <input
-        placeholder="Last Name"
-        onChange={(event) => {
-          setUser({ ...user, lastName: event.target.value });
-        }}
-      />
-      <input
-        placeholder="Username"
-        onChange={(event) => {
-          setUser({ ...user, username: event.target.value });
-        }}
-      />
-      <input
-        placeholder="Password"
-        type="password"
-        onChange={(event) => {
-          setUser({ ...user, password: event.target.value });
-        }}
-      />
-      <button onClick={signUp}> Sign Up</button>
     </div>
   );
 }
